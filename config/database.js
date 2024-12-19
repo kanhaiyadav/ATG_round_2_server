68341f6525b57f0fdf1ff48ae2a5b01a3ff2acdd
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2024-12-19 23:53:58 +0530

    little changes

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ async function main() {
     try {
         // await mongoose.connect("mongodb://localhost:27017/ATG_social_media");
         await mongoose.connect(
-            `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ic9en.mongodb.net/ATG`
+            `mongodb+srv://kanhaiya2004yadav:lW9VsZTOpuDVR7BX@cluster0.ic9en.mongodb.net/ATG`
         );
         console.log("Database connection successful...");
     } catch (err) {
