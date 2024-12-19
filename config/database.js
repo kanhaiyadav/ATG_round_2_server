03f7f98613e65c29f4f9bdba12970bfbfbb66c67
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2024-12-19 22:55:10 +0530

    connect to mongodb atlas

--- config/database.js
+++ config/database.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 main().catch((err) => console.log(err));
 
 async function main() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/ATG_social_media");
+        // await mongoose.connect("mongodb://localhost:27017/ATG_social_media");
+        await mongoose.connect(
+            `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ic9en.mongodb.net/ATG`
+        );
         console.log("Database connection successful...");
     } catch (err) {
         console.error("Problem connecting to the database", err); 
