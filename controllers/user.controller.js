a27bcb277524c07e944dd60dcd37bacf0d9ead19
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2024-12-20 08:12:47 +0530

    create reset password api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import jwt from 'jsonwebtoken';
+import ForgotUser from "../models/forgotUser.model.js";
 
 export const signUp = async (req, res) => {
     try {
@@ -43,5 +44,74 @@ export const signIn = async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).send(error.message);
+    }
+};
+
+export const forgotPassword = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({ message: "User not found!" });
+        } else {
+            console.log("hello");
+            const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: "10m" });
+            await ForgotUser.deleteMany({ email: user.email });
+            await ForgotUser.create({ email: user.email, token });
+            const forgotUser = await ForgotUser.findOne({ email: user.email });
+            console.log(forgotUser);
+            res.status(200).json({
+                success: true,
+                message: "Reset token created successfully",
+                data: {
+                    name: user.name,
+                    email: user.email,
+                    token,
+                }
+            });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const verify = async (req, res) => {
+    try {
+        const token = req.query.token;
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(400).json({ message: "Invalid or expired token" });
+            }
+            const forgotUser = ForgotUser.findOne({ email: decoded.email });
+            if (!forgotUser) {
+                return res.status(400).json({ message: "Invalid token" });
+            } else {
+                forgotUser.varified = true;
+                res.redirect('http://localhost:5173/auth/reset-password');
+            }
+        });
+    } catch {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const resetPassword = async (req, res) => {
+    try {
+        const forgotUser = await ForgotUser.findOne({ email: req.body.email });
+        let email;
+        jwt.verify(forgotUser.token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(400).json({ message: "Invalid or expired token" });
+            }
+            email = decoded.email;
+        });
+        const user = await User.findOne({ email: email });
+        user.password = req.body.newPassword;
+        await user.save();
+        await ForgotUser.deleteOne({ email: req.body.email });
+        res.status(200).json({ message: "Password reset successfully" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
     }
 };
